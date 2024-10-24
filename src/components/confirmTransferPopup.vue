d3237ffbf06b08e697857905de8208b963067ffd
Nbellavitis <nbellavitisalzate@itba.edu.ar>
2024-10-23 22:45:49 -0300

    ?!

--- src/components/confirmTransferPopup.vue
+++ src/components/confirmTransferPopup.vue
@@ -36,16 +36,17 @@ defineExpose({ showPopup })
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  z-index: 1000;
 }
 
 .popup-content {
-  background: #D6D6D6;
+  background-color: white;
   padding: 20px;
   border-radius: 8px;
   text-align: center;
